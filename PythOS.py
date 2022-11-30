3419d09c3dd2e4f55f0ddc12277ef0fef4aa6482
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-11-30 17:11:05 +1100

    Add files via upload

--- PythOS.py
+++ PythOS.py
@@ -6,10 +6,10 @@ Created on Sun Aug 29 13:08:33 2021
 @author: Charlie K.'Captain Awesome Junior'
 """ 
 
-# Changes in PythOS Indev 2.1.0: Added a logout option.
+# Changes in PythOS Indev 2.2.0: Added PythOS PRO (BETA)
 # To add the wait functionality, we need to import time.sleep.
 from time import sleep
-PythOS_Ver = "Indev 2.1.0"
+PythOS_Ver = "Alpha 1.0.0"
 # Here is the name data in rows 18-19.
 print("PythOS - The new way to use Python.")
 ProcessorType = 1
@@ -17,6 +17,7 @@ print("Running PythOS on AMD64")
 fname = ""
 sname = ""
 TryNewDev = ""
+ActivatedPyPro = False
 def login():
     print()
     global fname
@@ -48,8 +49,6 @@ load()
 # Now PythOS has loaded, we can create the actual operating system. But, we need to make a loop so that you can type in a command and then type in a new one.
 
 while ShutDown == False:
-    
-    print(TryNewDev)
     if TryNewDev == "Y":
            print("PythOS is shutting down so you can try PythOS Developer's Edition. Please wait...")
            sleep(5)
@@ -62,6 +61,8 @@ while ShutDown == False:
         print("power - Brings up power options")
         print("ver - Brings up info about your version of PythOS")
         print("settings - Lets you manage settings")
+        if ActivatedPyPro == True:
+            print("pro - Starts the PythOS Pro Workspace")
         print("launch - Opens the App Lancher, where you can open apps")
         UtillityOption = input("Type in your option: ")
         if UtillityOption == "power":
@@ -105,11 +106,67 @@ Quick Shutdown inisilisation. Press ENTER to return to main menu.""")
         elif UtillityOption == "ver":
                         print("PythOS", PythOS_Ver)
                         print("CONFIDETIONAL PROPERTY! If leaked, please email me at captainawesomejnr@outlook.com.au .")
-                        print("(C) Captainawesomejnr 2022. All rights reserved.")    
+                        print("(C) Captainawesomejnr 2022. All rights reserved.")
+        elif UtillityOption == "pro":
+            if ActivatedPyPro == False:
+                        print("Hmmm... It looks like PythOS Pro cannot start.")
+                        sleep(5)
+                        if ProcessorType == 1:
+                            STOPCRASH_Run = input("""STOP!
+
+A fatal error has occured and PythOS could not keep running. Due to this, PythOS was shut down to prevent damage to your installation.
+Fatal error cause: PythOS Pro could not start.
+             Code: x64-01010101-01110011-01100101-01110010
+You can:
+Press ENTER to terminate the pogram. It is recomended that you do this as it is the most effective fix.
+                OR
+Type 'close' to exit PythOS. You should not do this because it will abandon all unsaved data.
+
+For more information, check the article about STOP! crash screens at https://www.github.com/Captain-Awesome-Jnr
+
+
+
+
+
+Type in your option: """)
+                        if STOPCRASH_Run == "":
+                            print("Terminating program...")
+                            sleep(2.5)
+                            print("Sucsessfully terminated program. Returning to PythOS.")
+                        else:
+                            break
+            elif ProcessorType == 2:
+                STOPCRASH_Run = input("""STOP!
+
+A fatal error has occured and PythOS could not keep running. Due to this, PythOS was shut down to prevent damage to your installation.
+Fatal error cause: User-Activated Fatal Crash
+             Code: x86-01010101-01110011-01100101-01110010
+You can:
+Press ENTER to terminate the pogram. It is recomended that you do this as it is the most effective fix.
+                OR
+Type 'close' to exit PythOS. You should not do this because it will abandon all unsaved data.
+
+For more information, check the article about STOP! crash screens at https://www.github.com/Captain-Awesome-Jnr
+
+
+
+
+
+Type in your option: """)
+                if STOPCRASH_Run == "":
+                    print("Terminating program...")
+                    sleep(2.5)
+                    print("Sucsessfully terminated program. Returning to PythOS.")
+                else:
+                    break
+            else:
+                import PyPRO as pro
+                pro.run
         elif UtillityOption == "settings":
                         print("SETTINGS")
                         print("Here you can manage settings. Here are the settings you can manage:")
                         print("1. Processor type")
+                        print("2. PythOS Pro Activation")
                         setting = input("Which setting would you like to modify? ")
                         if setting == "1":
                             print("Select processor type:")
@@ -117,13 +174,33 @@ Quick Shutdown inisilisation. Press ENTER to return to main menu.""")
                             print("2. 32-bit")
                             processor = input("Put in your option: ")
                             if processor == "1":
-                                print("Running PythOS on AMD64")
+                                print("Running PythOS on AMD64 or x64")
                                 ProcessorType = 1
                             if processor == "2":
                                     print("Running PythOS on x86")
                                     ProcessorType = 2
                             else:
                                 rerun = input("Error 3: Processor type selection didn't work. Maybe you made a typo? Press ENTER.")
+                        elif setting == "2":
+                            print("Here you can manage whether or not you want PythOS Pro.")
+                            print("WARNING: Do not turn on PythOS Pro unless you have followed the instructions in its README.txt. This can cause PythOS to crash to the extent you need to restart it!!!")
+                            activatepypro = input("""With PythOS Pro, you can:
+- Use the FIRST PythOS GUI
+- Enjoy games
+and MUCH, MUCH MORE!
+Would you like to turn PythOS Pro on? (Y/N)""")
+                            if activatepypro == "Y" or "y":
+                                sleep(0.5)
+                                ActivatedPyPro = True
+                                print("PythOS Pro is ready to be used.")
+                            elif activatepypro == "N" or "n":
+                                sleep(0.5)
+                                ActivatedPyPro = False
+                                print("PythOS Pro has been deactivated. Thank you for trying.")
+                            else:
+                                sleep(1.5)
+                                ActivatedPyPro = False
+                                print("You did not enter Y or N. For the saftey of PythOS, Pro has been deactivated.")
                         else:
                             rerun = input("Error 2: Setting not found. Press ENTER")
         elif UtillityOption == "launch":
@@ -138,7 +215,7 @@ Quick Shutdown inisilisation. Press ENTER to return to main menu.""")
                                     CalcOperation = input("What operation would you like to use? (* = Times, / = Divided By, ^ = Power) ")
                                     CalcSecondNumber = float(input("Type in the other number: "))
                                     if CalcOperation == "/" and CalcSecondNumber == 0:
-                                        print(CalcFirstNumber, "/ 0.0: Error processing (Could not divide by 0.)")
+                                        print(CalcFirstNumber, "/ 0: Error processing (Could not divide by 0.)")
                                     else:
                                         if CalcOperation == "+":
                                             print(CalcFirstNumber, "+", CalcSecondNumber, "=", CalcFirstNumber + CalcSecondNumber)
@@ -237,6 +314,7 @@ Type in your option: """)
                     print("Sucsessfully terminated program. Returning to PythOS.")
                 else:
                     break
-
+            elif UtillityOption == "$KILL$":
+                break
             else:
                 rerun = input("Error 0: The utillity you have typed is not a proper utillity option. Press ENTER")
